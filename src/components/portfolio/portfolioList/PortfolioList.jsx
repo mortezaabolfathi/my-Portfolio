2fc3df32f9ff93c412cde4feba799b087e8e367e
Morteza <morteza.abolfathi73@gmail.com>
2022-11-11 10:51:22 +0330

    create portfolio

---
+++ src/components/portfolio/portfolioList/PortfolioList.jsx
@@ -0,0 +1,16 @@
+import "./portfolioList.scss";
+
+const PortfolioList = ({title, selected, id, setSelected}) => {
+
+    const handelSelectedItem = (id) =>{
+        setSelected(id)
+    };
+
+    return ( 
+        <li className={selected===id  && "active"} onClick={()=>handelSelectedItem(id)}>
+            {title}
+        </li>
+     );
+}
+ 
+export default PortfolioList;
