43c7990dd0e2ce6ae7dcbc90f40892a39c21c849
Morteza <morteza.abolfathi73@gmail.com>
2022-11-12 18:22:04 +0330

    create works page and style this page

---
+++ src/components/works/DataWork.js
@@ -0,0 +1,20 @@
+const DataWork = [
+  {
+    id: 1,
+    title: "supporterPart",
+    description: "توضیحات ب توضیحات مختصری مجری کار مرتضی اب.الفتحی که برای این کگار به مدت دوماه با ت.جه حکخ  در باره این پروژه برای این کار",
+    project: "پروژه",
+    icon: "assets/mobile.png",
+    img: "assets/wordpress/support_web.png",
+  },
+  {
+    id: 2,
+    title: "stone",
+    description: "توضیحات مختصری در باره این پروژه برای این کار",
+    project: "پروژه",
+    icon: "assets/mobile.png",
+    img: "assets/wordpress/stone_web.png",
+  },
+];
+
+export default DataWork;
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,11 +1,53 @@
-import "./work.scss"
-
+import "./work.scss";
+import DataWork from "./DataWork";
+import { useState } from "react";
 const Work = () => {
-    return ( 
-        <div className="works" id="works">
+  const [dataWorkList, setDataWorkList] = useState(DataWork);
+  const [currentSlide,setCurrentSlide] = useState(0)
 
+  const handelClickSlide = (way)=>{
+    if(way==="left"){
+      setCurrentSlide(currentSlide>0 ? currentSlide -1 : 2)
+    }
+    if(way==="right"){
+      setCurrentSlide(currentSlide<dataWorkList.length - 1 ? currentSlide +1 : 0)
+    }
+  }
+  return (
+    <div className="works" id="works">
+      <div className="slider" style={{ transform : `translateX(-${currentSlide * 100}vw)`}}>
+        {dataWorkList.map((dataWork) => {
+          return (
+            <div className="container">
+              <div className="item">
+                <div className="right">
+                  <img src={dataWork.img} alt="" />
+                </div>
+                <div className="left">
+                  <div className="leftContainer">
+                    <div className="imgContainer">
+                      <img src={dataWork.icon} alt="" />
+                    </div>
+                    <h2>{dataWork.title}</h2>
+                    <p>{dataWork.description}</p>
+                    <span>{dataWork.project}</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="arrow">
+        <div className="left" onClick={()=>handelClickSlide("left")}>
+          <img src="assets/arrow.png" alt="" />
         </div>
-    );
-}
- 
+        <div className="right" onClick={()=>handelClickSlide("right")}>
+          <img src="assets/arrow.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Work;
