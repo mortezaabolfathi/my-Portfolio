5540a93145060f7fa2fbb74ae74d6585c6a69de5
Morteza <morteza.abolfathi73@gmail.com>
2022-11-13 18:42:38 +0330

    update contact CMP

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,12 +1,37 @@
- import React from 'react';
- import "./contact.scss";
+import React from "react";
+import "./contact.scss";
 
- const Contact = () => {
-    return (
-        <div className='contact' id="contact">
-            
+const Contact = () => {
+
+  return (
+    <div className="contact" id="contact">
+      <div className="right">
+        <div className="item-contact">
+          <a href="https://www.linkedin.com/in/morteza-abolfathi-232843238/">LinkDin</a>
+          <img src="assets/linkedin.png" alt="Linkedin icon" />
         </div>
-    );
- }
-  
- export default Contact;
+        <div className="item-contact">
+          <a href="https://join.skype.com/invite/BFtUFcnzzGBX">Skype</a>
+          <img src="assets/skype.png" alt="phone icon" />
+        </div>
+        <div className="item-contact">
+          <a href="https://github.com/mortezaabolfathi">Github</a>
+          <img src="assets/gitHub.png" alt="github icon" />
+        </div>
+        <div className="item-contact">
+          <a href="morteza.abolfathi73@gmail.com">morteza.abolfathi73@gmail.com</a>
+          <img src="assets/Gmail.png" alt="gmail icon" />
+        </div>
+        <div className="item-contact">
+          <a href="">شماره تماس 09359919333</a>
+          <img src="assets/phone.jpg" alt="phone icon" />
+        </div>
+      </div>
+      <div className="left">
+        <img src="assets/connect.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
